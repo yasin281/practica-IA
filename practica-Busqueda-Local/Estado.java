fe896739f7fd285d48ce0e051b5a8d9c75f74a3d
Yasin <146195255+yasin281@users.noreply.github.com>
2023-10-11 16:45:18 +0200

    Add initial code in file Estado

--- practica-Busqueda-Local/Estado.java
+++ practica-Busqueda-Local/Estado.java
@@ -1,1 +1,16 @@
-#Estado
+import java.util.ArrayList;
+import java.util.List;
+import Bicing;
+
+public class Estado{
+    
+    private Estacion estacionInicial;
+    private Estacion estacionDestino1;
+    private Estacion estacionDestino2; // Este campo es opcional
+    
+    private int bicicletasDestino1;
+    private int bicicletasDestino2;
+    
+    private double recorridoTotalKM;
+
+}
