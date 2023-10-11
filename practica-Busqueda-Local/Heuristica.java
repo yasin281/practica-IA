faf4f48bca512c9333f9ae7295f2903ed8c751ca
sOdreams <momin.miah@estudiantat.upc.edu>
2023-10-11 16:44:50 +0200

    Modify some java files

--- practica-Busqueda-Local/Heuristica.java
+++ practica-Busqueda-Local/Heuristica.java
@@ -1,1 +1,11 @@
-#Heuristica
+
+package Bicing;
+
+import aima.search.framework.HeuristicFunction;
+
+public class Heuristica implements HeuristicFunction {
+    @Override
+    public double getHeuristicValue(Object o) {
+        return 0;
+    }
+}
