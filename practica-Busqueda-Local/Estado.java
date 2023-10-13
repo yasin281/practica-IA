dcb164963d21b69220acc703da77737f71f37030
sOdreams <momin.miah@estudiantat.upc.edu>
2023-10-13 15:03:24 +0200

    Add 1) Implementation Problem

--- practica-Busqueda-Local/Estado.java
+++ practica-Busqueda-Local/Estado.java
@@ -1,16 +1,39 @@
 import java.util.ArrayList;
 import java.util.List;
 import IA.Bicing.Estacion;
+
 public class Estado{
     
-    private Estacion estacionInicial;
-    private Estacion estacionDestino1;
-    private Estacion estacionDestino2; // Este campo es opcional
+    //matriz de fx6 
+    private int[][] furgos;
+    //funcion para inicializar esta matriz
+    public void inicializarFurgos(int nfurg, int nest){
+        furgos = new int[nfurg][nest];
+        for(int i = 0; i < nfurg; ++i){
+            for(int j = 0; j < nest; ++j){
+                furgos[i][j] = 0;
+            }
+        }
+    }
     
-    private int bicicletasDestino1;
-    private int bicicletasDestino2;
+    //funcion para cada elemento de [][]furgo le asigne 6 enteros
+    public void setFurgos(int furg, int Est0,int Est1,int Est2, int B1, int B2, int Km){
+        furgos[furg][0] = Est0;
+        furgos[furg][1] = Est1;
+        furgos[furg][2] = Est2;
+        furgos[furg][3] = B1;
+        furgos[furg][4] = B2;
+        furgos[furg][5] = Km;
+    }
     
-    private double recorridoTotalKM;
-
-    private double distance;
+    //imprime la matriz de furgos
+    public void printFurgos(){
+        for(int i = 0; i < furgos.length; ++i){
+            for(int j = 0; j < furgos[0].length; ++j){
+                System.out.print(furgos[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
+
--- practica-Busqueda-Local/Main.java
+++ practica-Busqueda-Local/Main.java
@@ -1,4 +1,3 @@
-package Bicing;
 import IA.Bicing.Estaciones;
 import IA.Bicing.Estacion;
 import java.util.Iterator;
@@ -19,15 +18,28 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int nest = sc.nextInt();
         int nbic = sc.nextInt();
+        //nbic tiene que ser como minimo 50*nest, es menor da error
+        if(nest* 50 < nbic) {
+            System.out.println("Error: el numero de bicicletas es menor que 50*numero de estaciones");
+            System.exit(1);
+        }
+        int nfurg = sc.nextInt();
         int dem = sc.nextInt();
         int seed = sc.nextInt();
+
         //ahora pasamos estos datos a la clase estaciones
         Estaciones estaciones = new Estaciones(nest, nbic, dem, seed);
-        //itera sobre las estaciones y printea el numero de bicicletas por estacion
-        // Estaciones es una Array de estaciones, no hay getEstaciones
-        for(Estacion e : estaciones){
-            System.out.println(e.getDemanda());
-        }
+
+        Estado furgonetas = new Estado();
+        furgonetas.inicializarFurgos(nfurg, 6);
+        furgonetas.setFurgos(0, 0, 1, 2, 0, 0, 0);
+        furgonetas.setFurgos(1, 3, 4, 5, 0, 0, 0);
+        furgonetas.setFurgos(2, 6, 7, 8, 0, 0, 0);
+        furgonetas.setFurgos(3, 9, 10, 11, 0, 0, 0);
+        furgonetas.setFurgos(4, 12, 13, 14, 0, 0, 0);
+        furgonetas.printFurgos();
+
+
     }
 }
 
