2d6c0909a85898ccbc34c63112e041ca3b03b646
sOdreams <momin.miah@estudiantat.upc.edu>
2023-10-22 19:23:52 +0200

    Delete Some Files

--- practica-Busqueda-Local/prueba2/BicingEstadoSolucion.java
+++
@@ -1,9 +0,0 @@
-import aima.search.framework.GoalTest;
-
-public class BicingEstadoSolucion implements GoalTest {
-    @Override
-    public boolean isGoalState(Object aState) {
-    return(false);
-  }
-
-}
